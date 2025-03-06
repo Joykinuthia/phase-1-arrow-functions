5c3bdd4175ba110c3e716bd3cdd9f281439d813c
Joykinuthia <joyrosekinuthia@gmail.com>
2025-03-06 09:00:12 +0300

    arrow functions

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = (a ,b) => a/b 
+
+    let a = 2000;
+    let b = 100;
+    console.log(a,b);
+
+const square = x =>  (x * 2);
+    let x = 2;
+    console.log(x);
+
+const add = (a, b) => {
+    return a +b;
+}
+    console.log(add(3,4));
+
